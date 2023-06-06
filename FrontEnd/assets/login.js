96d507aa3be4dd1ad24c98ec993d04a423eff73f
jeremsers <jeremy.sersoub@hotmail.fr>
2023-06-06 06:26:48 +0200

    implemented API request for content and login page

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,23 @@
+const submitInfo = document.querySelector(".login-form")
+export let userToken = 0
+submitInfo.addEventListener("submit",  async function (event) {
+	event.preventDefault()
+	const login = {
+		email: event.target.querySelector('[name=email').value,
+		password: event.target.querySelector('[name=pass]').value
+	}
+	const reponse = await fetch('http://localhost:5678/api/users/login', {
+	method: "POST",
+	headers: {"content-type": "application/json"},
+	body:JSON.stringify(login)
+})
+if (reponse.status === 200){
+    const reponseObj = await reponse.json()
+    userToken = reponseObj.token 
+    window.localStorage.setItem("token", userToken)
+    location.href= "index.html"
+    
+}else {
+    alert('mauvais email ou mot de passe')
+}
+})
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,15 +1,77 @@
-
-
 const reponse = await fetch("http://localhost:5678/api/works");
 const works = await reponse.json();
-console.log(works[0]);
+
+if (window.localStorage.getItem('token') !== null){
+	
+
+	SetAdminLayout()
+	addEditButton()
+	logoutFunction()
+	removeFilter()
+
+}
+
+function removeFilter(){
+	const filterDiv = document.querySelector('.button-div')
+	filterDiv.remove()
+	const projetTitre = document.querySelector('.titre-projet')
+	projetTitre.classList.add('titre-projet--admin')
+}
+
+function addEditButton(){
+	//creation de la div contenat l'icone et le texte
+	const editDiv = document.createElement('div')
+	editDiv.className = 'edit-btn'
+	editDiv.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>'
+	const editText = document.createElement('p')
+	editText.innerText = "modifier"
+	editDiv.appendChild(editText)
+	//ajout de la div aux endroits nécessaires
+	const Parent1 = document.querySelector('.figure-accueil') 
+	const editdivPhoto = editDiv.cloneNode(true)
+	editdivPhoto.className = "edit-div-photo"
+	Parent1.append(editdivPhoto)
+	editDiv.className = "edit-div-title"
+	const Parent2 = document.querySelector('#portfolio')
+	Parent2.prepend(editDiv)
+	
+	
+
+}
+function SetAdminLayout(){
+	const adminParent = document.querySelector('body')
+	const adminElement = document.createElement('aside')
+	adminElement.className = "admin-div"
+	adminParent.prepend(adminElement)
+	const editDivEvelement = document.createElement('div')
+	editDivEvelement.className = "admin-div__editdiv"
+	editDivEvelement.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>'
+	const editText = document.createElement('p')
+	editDivEvelement.appendChild(editText)
+	editText.innerText ="Mode édition"
+	const buttonElement = document.createElement('button')
+	buttonElement.className = "admin-div__button"
+	buttonElement.innerText = "publier les changements"
+	adminElement.appendChild(editDivEvelement)
+	adminElement.appendChild(buttonElement)
+}
+
+function logoutFunction() {
+	const logoutBtn = document.querySelector('.login-btn')
+	logoutBtn.innerHTML = "logout"
+	logoutBtn.addEventListener("click", function(event){
+		event.preventDefault()
+		window.localStorage.removeItem('token')
+		location.reload()
+	})
+}
 
 function genererWork(works) {
 	const gallery = document.querySelector(".gallery");
 	gallery.innerHTML = "";
 	for (let i = 0; i < works.length; i++) {
 		const work = works[i];
-
+		
 		const figureElement = document.createElement("figure");
 		const imageElement = document.createElement("img");
 		imageElement.src = work.imageUrl;
@@ -27,7 +89,6 @@ function buttonClassReset() {
 	const allButton = document.querySelectorAll(".button");
 	allButton.forEach((element) => {
 		element.classList.remove("button_activated");
-		console.log(element.classList);
 	});
 }
 
@@ -43,7 +104,7 @@ objFiltre.addEventListener("click", function () {
 	buttonClassReset();
 	objFiltre.classList.add("button_activated");
     const workFilterObj = works.filter(function(work){
-        return work.categoryId == 1;
+		return work.categoryId == 1;
     })
 	genererWork(workFilterObj);
 });
@@ -51,10 +112,10 @@ objFiltre.addEventListener("click", function () {
 const appartementsFiltre = document.querySelector(".btn-appartements");
 appartementsFiltre.addEventListener("click", function () {
 	buttonClassReset();
-
+	
 	appartementsFiltre.classList.add("button_activated");
 	const workFilterObj = works.filter(function(work){
-        return work.categoryId == 2;
+		return work.categoryId == 2;
     })
 	genererWork(workFilterObj);
 });
@@ -65,23 +126,9 @@ hotresFiltre.addEventListener("click", function () {
 	buttonClassReset();
 	hotresFiltre.classList.add("button_activated");
 	const workFilterObj = works.filter(function(work){
-        return work.categoryId == 3;
+		return work.categoryId == 3;
     })
 	genererWork(workFilterObj);
 });
 
-const submitInfo = document.querySelector(".login-form")
-submitInfo.addEventListener("submit", function (event) {
-    console.log('yo')
-    event.preventDefault()
-    const login = {
-        email: event.target.querySelector("name=email").value,
-        password: event.target.querySelector("name=pass").value
-    }
-    const sendLogin = fetch('http://localhost:5678/api/users/login', {
-    method: "POST",
-    headers: {"content-type": "application/json"},
-    body:JSON.stringify(login)
-})
-console.log(sendLogin)
-})
+
